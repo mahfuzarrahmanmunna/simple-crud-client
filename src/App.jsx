562c0ef2deca2e7abf4b27eb6d7078995ba699cc
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2025-05-14 02:34:25 +0600

    added newuser in mongodb database

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import Users from './Components/Users'
+
+const userPromise = fetch('http://localhost:3000/users').then(res => res.json())
 
 function App() {
   return (
     <>
       <h1>Simple Crud Operation</h1>
+      <Users userPromise={userPromise} />
     </>
   )
 }
--- src/Components/Users.jsx
+++ src/Components/Users.jsx
@@ -1,9 +1,52 @@
-import React from 'react';
+import React, { use, useState } from 'react';
 
-const Users = () => {
+const Users = ({ userPromise }) => {
+    const initialUsers = use(userPromise)
+    console.log(initialUsers);
+    const [user, setUser] = useState(initialUsers)
+    console.log(user);
+
+    const handleAddUser = e => {
+        e.preventDefault()
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const newUser = { name, email }
+        console.log(newUser);
+
+        // create User 
+        fetch('http://localhost:3000/users', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(newUser),
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log('data after creating user in the db', data);
+                if (data.insertedId) {
+                    newUser._id = data.insertedId;
+                    const newUsers = [...user, newUser]
+                    setUser(newUsers)
+                    alert("User added successfully");
+                    e.target.reset()
+                }
+            })
+    }
     return (
         <div>
-
+            <div>
+                <form onSubmit={handleAddUser}>
+                    <input type="text" name="name" id="" /><br />
+                    <input type="email" name="email" id="" /><br />
+                    <input type="submit" value="Add User" />
+                </form>
+            </div>
+            <div>
+                {user?.map(user => (
+                    <p key={user._id}>{user.name} : {user.email}</p>
+                ))}
+            </div>
         </div>
     );
 };
